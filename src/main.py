1a7425f08448008c52df92ed255769fb35c5bcfa
Nathan Woodruff <nwoodr94@gmail.com>
2020-04-04 23:02:01 -0700

    write listings to /dist

--- src/main.py
+++ src/main.py
@@ -29,44 +29,49 @@ driver.get(url)
 content = driver.page_source
 soup = BeautifulSoup(content, 'html.parser')
 
-# Get 4Runner links, date posted, price, and distance from zip
-links = []
-dates = []
-prices = []
-distances = []
+
+# Get 4Runner links, date posted, price, distance, and uid
+link, date, price, distance, uid = [], [], [], [], []
 
 for result in soup.find_all('p', attrs={'class': 'result-info'}):
-    links += [result.find('a').get('href')]
+    link += [result.find('a').get('href')]
 
 for result in soup.find_all('time', attrs={'class': 'result-date'}):
-    dates += [result.get_text()]
+    date += [result.get_text()]
 
 for result in soup.find_all('span', attrs={'class': 'result-price'}):
-    prices += [result.get_text()]
+    price += [result.get_text()]
 
 for result in soup.find_all('span', attrs={'class': 'maptag'}):
-    distances += [result.get_text()]
+    distance += [result.get_text()]
+
+for result in soup.find_all('li', attrs={'class': 'result-row'}):
+    uid += [result.get('data-pid')]
+
 
 # Iterate through each listing, and append each property to a list of attributes
 runners = {}
-for runner in range(len(links)):
-    attribute_list = [dates[runner], prices[runner], distances[runner]]
+for runner in range(len(link)):
+    attribute_list = [date[runner], price[runner], distance[runner]]
 
-    # Listing details are available only on its respective source code
-    driver.get(links[runner])
+    driver.get(link[runner])
     listing = driver.page_source
     soup = BeautifulSoup(listing, 'html.parser')
 
-    #There are two attribute groups containing useful span elements 
-    for group in soup.find_all('p', attrs={'class': 'attrgroup'}):
+    # There are at most 2 attribute groups containing useful span elements 
+    for group in soup.find_all('p', attrs={'class': 'attrgroup'}, limit=2):
         for span in group.find_all('span'):
             prop = span.text
             attribute_list += [prop]
         
-    attribute_list += [links[runner]]
+    attribute_list += [link[runner], uid[runner]]
     
     # Each listing is an index in the runners dictionary
     runners[runner] = attribute_list
 
-print(runners[0])
-# ['Mar 19', '$4900', '15.9mi', '2005 toyota 4runner', 'condition: good', 'cylinders: 6 cylinders', 'drive: 4wd', 'fuel: gas', 'title status: clean', 'transmission: automatic', 'https://losangeles.craigslist.org/wst/cto/d/carson-2005-toyota-4runner-sr5-4x4/70959']
+
+# Write listings into a deliverable in /dist
+with open("./dist/listings.txt", "w+") as f: 
+    for i in runners:
+        f.write('\n'.join(runners[i])+'\n\n')
+    f.close()
